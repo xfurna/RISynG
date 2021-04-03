6a7ad1c5bc50c93030088a12dec0261f5844bdbc
evi1haxor <architdwivedi.off@gmail.com>
2021-04-03 17:02:08 +0530

    plo.svg

--- plotting/3dplot.py
+++ plotting/3dplot.py
@@ -59,25 +59,40 @@ s=20
 c1=[np.sum(u[np.where(tr==1),0])/u[np.where(tr==1),0].shape[1],np.sum(u[np.where(tr==1),1])/u[np.where(tr==1),0].shape[1],np.sum(u[np.where(tr==1),2])/u[np.where(tr==1),0].shape[1]]
 c2=[np.sum(u[np.where(tr==2),0])/u[np.where(tr==2),0].shape[1],np.sum(u[np.where(tr==2),1])/u[np.where(tr==2),0].shape[1],np.sum(u[np.where(tr==2),2])/u[np.where(tr==2),0].shape[1]]
 c3=[np.sum(u[np.where(tr==3),0])/u[np.where(tr==3),0].shape[1],np.sum(u[np.where(tr==3),1])/u[np.where(tr==3),0].shape[1],np.sum(u[np.where(tr==3),2])/u[np.where(tr==3),0].shape[1]]
-# c4=[np.sum(u[np.where(tr==4),0]),np.sum(u[np.where(tr==4),1]),np.sum(u[np.where(tr==4),2])]
+if 4 in tr:
+    c4=[np.sum(u[np.where(tr==4),0])/u[np.where(tr==4),0].shape[1],np.sum(u[np.where(tr==4),1])/u[np.where(tr==4),0].shape[1],np.sum(u[np.where(tr==4),2])/u[np.where(tr==4),0].shape[1]]
 
-red='lightcoral'
-blue='cornflowerblue'
-green='yellowgreen'
-ax.scatter3D(u[np.where(tr==1),0],u[np.where(tr==1),1],u[np.where(tr==1),2],c=red,s=s,marker='o')
-ax.scatter3D(u[np.where(tr==2),0],u[np.where(tr==2),1],u[np.where(tr==2),2],c=green,s=s,marker='v')
-ax.scatter3D(u[np.where(tr==3),0],u[np.where(tr==3),1],u[np.where(tr==3),2],c=blue,s=s,marker='s')
-ax.scatter3D(u[np.where(tr==4),0],u[np.where(tr==4),1],u[np.where(tr==4),2],c='orange',s=s,marker='p')
+p1={}
+p1['red']='lightcoral'
+p1['blue']='cornflowerblue'
+p1['green']='yellowgreen'
+p1['purple']='orchid'
 
-for (x,y,z) in zip(u[np.where(tr==1),0][0],u[np.where(tr==1),1][0],u[np.where(tr==1),2][0]):
-    plt.plot([x,c1[0]],[y,c1[1]],[z,c1[2]],red)
-for (x,y,z) in zip(u[np.where(tr==2),0][0],u[np.where(tr==2),1][0],u[np.where(tr==2),2][0]):
-    plt.plot([x,c2[0]],[y,c2[1]],[z,c2[2]],green)
-for (x,y,z) in zip(u[np.where(tr==3),0][0],u[np.where(tr==3),1][0],u[np.where(tr==3),2][0]):
-    plt.plot([x,c3[0]],[y,c3[1]],[z,c3[2]],blue)
+p2={}
+p2['red']='r'
+p2['blue']='b'
+p2['green']='g'
+p2['purple']='orchid'
+
+
+ax.scatter3D(u[np.where(tr==1),0],u[np.where(tr==1),1],u[np.where(tr==1),2],c=p2['red'],s=s,marker='o')
+ax.scatter3D(u[np.where(tr==2),0],u[np.where(tr==2),1],u[np.where(tr==2),2],c=p2['green'],s=s,marker='v')
+ax.scatter3D(0.1+u[np.where(tr==3),0],u[np.where(tr==3),1],u[np.where(tr==3),2],c=p2['blue'],s=s,marker='s')
+ax.scatter3D(u[np.where(tr==4),0],u[np.where(tr==4),1],u[np.where(tr==4),2],c=p2['purple'],s=s,marker='p')
+
+# for (x,y,z) in zip(u[np.where(tr==1),0][0],u[np.where(tr==1),1][0],u[np.where(tr==1),2][0]):
+#     plt.plot([x,c1[0]],[y,c1[1]],[z,c1[2]],p1['red'])
+# for (x,y,z) in zip(u[np.where(tr==2),0][0],u[np.where(tr==2),1][0],u[np.where(tr==2),2][0]):
+#     plt.plot([x,c2[0]],[y,c2[1]],[z,c2[2]],p1['green'])
+# for (x,y,z) in zip(u[np.where(tr==3),0][0],u[np.where(tr==3),1][0],u[np.where(tr==3),2][0]):
+#     plt.plot([x,c3[0]],[y,c3[1]],[z,c3[2]],p1['blue'])
+
+
 # plt.scatter(u[np.where(tr==1),0],u[np.where(tr==1),1],s=s,marker='o')
 # plt.scatter(u[np.where(tr==2),0],u[np.where(tr==2),1],s=s,marker='v')
 # plt.scatter(u[np.where(tr==3),0],u[np.where(tr==3),1],s=s,marker='s')
 # plt.scatter(u[np.where(tr==4),0],u[np.where(tr==4),1],s=s,marker='p')
 plt.axis('off')
+from matplotlib import rcParams
+rcParams['figure.dpi'] = 500
 plt.show()
